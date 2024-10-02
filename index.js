a7672a049354cad0a7484af3e6da5d0d9cf24149
sanusi1995 <samsonsanusio@gmail.com>
2024-10-02 12:21:31 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const submit = document.getElementById("click");
+const external_container =document.getElementById("preview");
+const errormessage =document.getElementById("error");
+
+console.log(error);
+
+
